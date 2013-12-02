209b3fe9354cd5517b488f7c428743f221f151a5
Duncan Bayne <dhgbayne@gmail.com>
2013-12-02 16:06:16 +1100

    I use Firefox these days.

--- santa.rb
+++ santa.rb
@@ -2,17 +2,17 @@
 
 # Secret Santa - a tool to randomly select Christmas gifts from eBay.
 # Copyright (C) 2012 "Duncan Bayne" <dhgbayne@gmail.com>
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
@@ -30,7 +30,7 @@ SEARCH_OPTIONS = 'LH_FS=1&_udlo=4&LH_BIN=1&_mPrRngCbx=1&_udhi=5&LH_PrefLoc=1&_ar
 PRESENTS = 13
 
 # browser executable to use
-BROWSER = 'chromium-browser'
+BROWSER = 'firefox'
 
 def get_category_document category
   page = RestClient.get "#{category}?#{SEARCH_OPTIONS}"
@@ -50,7 +50,7 @@ def get_first_filtered_item_in category
 end
 
 def remove_overseas_nodes doc
-  doc.xpath('//div[@class="tp"]').each do |node| 
+  doc.xpath('//div[@class="tp"]').each do |node|
     node.remove
     $stderr.puts "santa.rb: removed overseas sellers from page"
   end
